fcca36e2994a0ae3a46ca00d2d89b5ea869151ee
valhio <valentingerasimow@gmail.com>
2023-12-09 15:24:18 +0200

    Added documentation to what the prismadb file does

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
+// What this file does is it creates a global variable called prisma, which is an instance of PrismaClient. This is so that we can use the same instance of PrismaClient throughout our application. This is important because PrismaClient is a singleton, which means that it should only be instantiated once per application. If we don't do this, we will get an error saying that we are instantiating PrismaClient more than once.
 declare global {
     var prisma: PrismaClient | undefined;
 }
