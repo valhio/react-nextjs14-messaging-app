201d698e8086b3f2f22bdd81dbad8956847f3991
valhio <valentingerasimow@gmail.com>
2023-12-11 18:53:19 +0200

    added session to the entire app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import ToasterContext from './context/ToasterContext'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'RilaTalks',
-  description: 'RilaTalksMessanger',
-}
+  title: "RilaTalks",
+  description: "RilaTalksMessanger",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
-        </body>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
     </html>
-  )
+  );
 }
