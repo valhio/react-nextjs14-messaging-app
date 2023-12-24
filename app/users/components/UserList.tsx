ab36237fcb371b5658689be64933bfa60920e77d
valhio <valentingerasimow@gmail.com>
2023-12-24 03:36:24 +0200

    ui touch-ups

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -11,9 +11,11 @@ const UserList: React.FC<UserListProps> = ({ users }) => {
   return (
     <aside
       className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-y-auto border-r border-gray-200 block w-full left-0">
-      <div className="px-5 py-2">
-        <div className="flex-col">
-          <div className="text-lg font-bold text-gray-800 mb-5">Users</div>
+      <div className="px-4">
+          <div className="flex justify-between my-3 ">
+            <div className="text-lg flex items-center font-bold text-neutral-800 pointer-events-none">
+              Users
+            </div>
         </div>
         {users.map((user) => (
           <UserListItem key={user.id} data={user} />
